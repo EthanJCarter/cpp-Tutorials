70f1f45f5b555fba06044c4cd139922036eb545c
EthanJCarter <Ethan.J.Carter@outlook.com>
2026-08-02 23:24:59 +0100

    added some examples of variable initialisation.

--- 1_3_var_declaration/main.cpp
+++ 1_3_var_declaration/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-// function that declared int var x and returns it.
+// function that declares int var x and returns it.
 int declareIntVar()
 {
     int x = 42; // declare variable named x as an integer.
@@ -13,5 +13,25 @@ int main()
     int x = declareIntVar();
     // again, using stream output format here.
     std::cout << "x is: " << x;
+
+    // below are some initialisation methods.
+
+    // default
+    int a;
+    a = 1;
+
+    // copy
+    int b = 2;
+
+    // direct initialisation
+    int c ( 3 );
+
+    // direct list initialisation
+    int d {4};
+    
+    std::cout << "\na is: " << a << "\n";
+    std::cout << "b is: " << b << "\n";
+    std::cout << "c is: " << c << "\n";
+    std::cout << "d is: " << d << "\n";
     return 0;
-}
+}   
